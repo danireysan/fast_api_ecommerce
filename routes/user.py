9f635fdf635ffe178dbf6a79060bfe955fad4714
Daniel <danireysan@gmail.com>
2022-06-01 09:43:02 -0500

    first commit

---
+++ routes/user.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+user = APIRouter()
+
+user.get("/users")
+def helloworld():
+    return "helloworld"
+user.get("/users")
+def helloworld():
+    return "helloworld"
+user.get("/users")
+def helloworld():
+    return "helloworld"
+user.get("/users")
+def helloworld():
+    return "helloworld"
